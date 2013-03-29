eaa737575a9b7356c612dc8ad83a8003473d9c93
andy bristol <andbristol@gmail.com>
2013-03-29 12:11:32 -0400

    move some lines around

--- person.php
+++ person.php
@@ -3,15 +3,14 @@
     require("config.php");
 
     $mysqli = new mysqli($mnconfig["host"], $mnconfig["user"], $mnconfig["password"], $mnconfig["db"]);
-
-    $pid = $_GET["pid"];
-
-    $statement = "SELECT * FROM People WHERE pid=$pid"
-
     if ($db->connect_errno) {
         echo "Failed to connect to db: $db->connect_errno $db->connect_error";
         exit(1);
     }
+
+    $pid = $_GET["pid"];
+
+    $statement = "SELECT * FROM People WHERE pid=$pid";
 
     $res = $db->query($statement);
 
