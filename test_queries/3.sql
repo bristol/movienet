aee47ef361d0b583eb4cf307186d20e05d5ddc9d
bristol <andbristol@gmail.com>
2013-04-06 20:10:07 -0400

    added no cache statements to selections

--- test_queries/3.sql
+++ test_queries/3.sql
@@ -1,4 +1,4 @@
-create temporary table steves as (select pid from people where name like 'Spielberg, Steven');
-create temporary table steve_movies as (select distinct mid from directed where pid in steves);
-create temporary table steve_actors as (select distinct pid from acted_in where mid in steve_movies);
-select * from people where pid in steve_actors;
+create temporary table steves as (select SQL_NO_CACHE pid from people where name like 'Spielberg, Steven');
+create temporary table steve_movies as (select SQL_NO_CACHE distinct mid from directed where pid in steves);
+create temporary table steve_actors as (select SQL_NO_CACHE distinct pid from acted_in where mid in steve_movies);
+select SQL_NO_CACHE * from people where pid in steve_actors;
--- test_queries/5.sql
+++ test_queries/5.sql
@@ -1,4 +1,4 @@
-create temporary table derricks as (select uid from users where name like 'DERRICK MYERS' and age=36);
-create temporary table derrick_movies as (select distinct mid from rated where rating=10 and uid in derricks);
-create temporary table derrick_directors as (select distinct pid from directed where mid in derrick_movies);
-select name from people where pid in derrick_directors;
+create temporary table derricks as (select SQL_NO_CACHE uid from users where name like 'DERRICK MYERS' and age=36);
+create temporary table derrick_movies as (select SQL_NO_CACHE distinct mid from rated where rating=10 and uid in derricks);
+create temporary table derrick_directors as (select SQL_NO_CACHE distinct pid from directed where mid in derrick_movies);
+select SQL_NO_CACHE name from people where pid in derrick_directors;
