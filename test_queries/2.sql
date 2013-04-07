6fc51e234250bae5586e4a9ded099d5e669b6ab8
bristol <andbristol@gmail.com>
2013-04-06 21:07:23 -0400

    improved the performance of this by a whole lot by just using joins

--- test_queries/2.sql
+++ test_queries/2.sql
@@ -1,3 +1,3 @@
 create temporary table brads as (select SQL_NO_CACHE pid from people where name like 'Pitt, Brad');
-create temporary table brad_movies as (select SQL_NO_CACHE distinct mid from acted_in where pid in brads);
-select SQL_NO_CACHE * from movies where mid in brad_movies;
+create temporary table brad_movies as (select SQL_NO_CACHE distinct mid from brads natural join acted_in);
+select SQL_NO_CACHE * from brad_movies natural join movies;
