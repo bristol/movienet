465266488dd5697c2b9bfc87670f705296bd3c44
bristol <andbristol@gmail.com>
2013-04-06 21:20:00 -0400

    changed this one to use joins

--- test_queries/3.sql
+++ test_queries/3.sql
@@ -1,4 +1,4 @@
 create temporary table steves as (select SQL_NO_CACHE pid from people where name like 'Spielberg, Steven');
-create temporary table steve_movies as (select SQL_NO_CACHE distinct mid from directed where pid in steves);
-create temporary table steve_actors as (select SQL_NO_CACHE distinct pid from acted_in where mid in steve_movies);
-select SQL_NO_CACHE * from people where pid in steve_actors;
+create temporary table steve_movies as (select SQL_NO_CACHE distinct mid from steves natural join directed);
+create temporary table steve_actors as (select SQL_NO_CACHE distinct pid from steve_movies natural join acted_in);
+select SQL_NO_CACHE * from steve_actors natural join people;
