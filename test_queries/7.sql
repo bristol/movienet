cb0bfee7807dc7ca20c30a77f74c6b91c1fb50c0
bristol <andbristol@gmail.com>
2013-04-08 23:02:25 -0400

    added no cache i think whatever

--- test_queries/7.sql
+++ test_queries/7.sql
@@ -1,6 +1,6 @@
-create temporary table bad_kids as (select U.uid
+create temporary table bad_kids as (select SQL_NO_CACHE U.uid
 					from users U, rated R, movies M, has_mpaa H, mpaaratings A
 					where U.uid=R.uid and R.mid=M.mid and M.mid=H.mid and H.mpaaid=A.mpaaid and A.abbreviation like 'NC-17' and U.age <= 17);
-select U.email, U.name, U.age, U.location
+select SQL_NO_CACHE U.email, U.name, U.age, U.location
 from bad_kids B, users U
 where B.uid=U.uid;
