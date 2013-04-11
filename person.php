03255b080a98a47a7965c8325a022a79168f1105
andy bristol <andbristol@gmail.com>
2013-04-11 12:51:00 -0400

    rename the db connectiom variable correctly

--- person.php
+++ person.php
@@ -2,7 +2,7 @@
 
     require("config.php");
 
-    $mysqli = new mysqli($mnconfig["host"], $mnconfig["user"], $mnconfig["password"], $mnconfig["db"]);
+    $db = new mysqli($mnconfig["host"], $mnconfig["user"], $mnconfig["password"], $mnconfig["db"]);
     if ($db->connect_errno) {
         echo "Failed to connect to db: $db->connect_errno $db->connect_error";
         exit(1);
