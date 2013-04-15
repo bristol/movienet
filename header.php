9564c911fbfff5ab6e47781f63087ed35d327174
andy bristol <andbristol@gmail.com>
2013-04-15 16:42:57 -0400

    the header for our site

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class='navbar navbar-inverse navbar-fixed-top'>
+    <div class='navbar-inner'>
+        <a class='brand' href='#'>Movienet</a>
+        <ul class='nav'>
+            <li class='active'>
+                <a href='index.php'>Home</a>
+            </li>
+            <li>
+                <a href='user.php'>Profile</a>
+            </li>
+        </ul>
+    </div>
+</div>
