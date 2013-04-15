f4b83b00735c6624d115db7a6ddd4f10c329a065
andy bristol <andbristol@gmail.com>
2013-04-15 19:09:29 -0400

    closed div tag and added a name for the search input

--- person.php
+++ person.php
@@ -27,9 +27,10 @@
         $searchfield = "<div class='hero-unit'>\n";
         $searchfield .= "<p>Search for actors, directors, and producers</p>\n";
         $searchfield .= "<form class='form-search'>\n";
-        $searchfield .= "<input type='text' class='input-xxlarge' placeholder='Name of actor, director, or producer'>\n";
+        $searchfield .= "<input type='text' name='person' class='input-xxlarge' placeholder='Name of actor, director, or producer'>\n";
         $searchfield .= "<button type='submit' class='btn btn-primary'>Search</button>\n";
         $searchfield .= "</form>\n";
+        $searchfield .= "</div>\n";
         echo $searchfield;
     }
 
