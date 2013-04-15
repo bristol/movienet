44bf0cc1314001b3d32184e0e7604f9585212bb1
andy bristol <andbristol@gmail.com>
2013-04-15 18:26:54 -0400

    fill out navbar, get rid of active class

--- header.php
+++ header.php
@@ -1,12 +1,18 @@
 <div class='navbar navbar-inverse navbar-fixed-top'>
     <div class='navbar-inner'>
-        <a class='brand' href='#'>Movienet</a>
+        <a class='brand' href='index.php'>Movienet</a>
         <ul class='nav'>
-            <li class='active'>
+            <li>
                 <a href='index.php'>Home</a>
             </li>
             <li>
-                <a href='user.php'>Profile</a>
+                <a href='movie.php'>Movies</a>
+            </li>
+            <li>
+                <a href='person.php'>Artists</a>
+            </li>
+            <li>
+                <a href='user.php'>Me</a>
             </li>
         </ul>
     </div>
