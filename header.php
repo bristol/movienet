49d2e1497c05ef3ecc62abfbb10643ed35ee535a
bristol <andbristol@gmail.com>
2013-04-24 15:52:45 -0400

    put the login form in the header and set it up so logged in users see a link to their user page

--- header.php
+++ header.php
@@ -3,17 +3,40 @@
         <a class='brand' href='index.php'>Movienet</a>
         <ul class='nav'>
             <li>
-                <a href='index.php'>Home</a>
-            </li>
-            <li>
                 <a href='movie.php'>Movies</a>
             </li>
             <li>
                 <a href='person.php'>Artists</a>
             </li>
-            <li>
-                <a href='user.php'>Me</a>
-            </li>
+            <?php 
+		if (isset($_COOKIE["username"])) {
+			echo "<li> <a href='user.php?u=" . $_COOKIE["uid"] . "'> " . $_COOKIE["username"] . " </a> </li> ";
+		}
+		?>
+
         </ul>
+
+	<?php
+
+		if (isset($_COOKIE["username"])) {
+
+			$banner = "<form method='post' class='navbar-form pull-right'> ";
+			$banner .= "<input name='deletecookie' value='true' type='hidden'> ";
+			$banner .= "<button type='submit' class='btn'>Log out</button> ";
+			$banner .= "</form>";
+
+			echo $banner;
+		} else {
+
+			$banner = "<form method='post' class='navbar-form pull-right'> ";
+              		$banner .= "<input name='email' class='span3' type='text' placeholder='Email'> ";
+              		$banner .= "<input name='password' class='span3' type='password' placeholder='Password'> ";
+              		$banner .= "<button type='submit' class='btn'>Log in</button> ";
+			$banner .= "</form> ";
+
+			echo $banner;
+		}
+
+	?>
     </div>
 </div>
