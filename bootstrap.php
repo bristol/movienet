f8b8c2b8ac7a917d6b09825bae2ce0d5ec9daee1
bristol <andbristol@gmail.com>
2013-04-25 11:50:16 -0400

    added a little extra style to pad the body so the navbar doesnt cut it off

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,7 @@
 <link rel='stylesheet' href='http://cs445.cs.umass.edu/www/cs445_3_s13/bootstrap/css/bootstrap.min.css'>
 <script src='http://cs445.cs.umass.edu/www/cs445_3_s13/bootstrap/css/bootstrap.min.css'></script>
+<style>
+	body {
+		padding-top: 60px;
+	}
+</style>
