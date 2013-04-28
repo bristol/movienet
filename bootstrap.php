7efdde606a0fab62f53762d886da9ded1c213c9e
bristol <andbristol@gmail.com>
2013-04-28 16:55:47 -0400

    added jquery

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,6 @@
 <link rel='stylesheet' href='http://cs445.cs.umass.edu/www/cs445_3_s13/bootstrap/css/bootstrap.min.css'>
 <script src='http://cs445.cs.umass.edu/www/cs445_3_s13/bootstrap/css/bootstrap.min.css'></script>
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
 <style>
 	body {
 		padding-top: 60px;
