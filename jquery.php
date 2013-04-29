eee739ed456dbdb36edd91e4e06596dcea5670a6
bristol <andbristol@gmail.com>
2013-04-29 17:45:54 -0400

    made the show/hide behavior for search forms faster

--- jquery.php
+++ jquery.php
@@ -1,14 +1,14 @@
 <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
 <script>
 	$(".search-hide").click(function() {
-		$(this).parent().parent().hide(500);
+		$(this).parent().parent().hide(300);
 	});
 	$("#search-toggle").click(function() {
-		$("#search-simple").toggle(500);
-		$("#search-advanced").toggle(500);
+		$("#search-simple").toggle(300);
+		$("#search-advanced").toggle(300);
 	});
 	$("#search-field-select").change(function() {
-		$("#" + $(this).val()).show(500);
+		$("#" + $(this).val()).show(300);
 		$(this).val("default");
 	});
 </script>
