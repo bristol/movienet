4837f58426e4b6419aa70f556a199ebba7494019
bristol <andbristol@gmail.com>
2013-04-30 01:16:26 -0400

    finished search for users

--- user.php
+++ user.php
@@ -98,191 +98,72 @@
 	<?php
 
 	if (isset($_GET["advanced"])) {
-		$select = array();
-		$from = array();
+
 		$where = array();
-		$having = array();
-		$groupby = array();
-		$orderby = array();
-		$limit = 0;
+		$terms = array();
 
-		array_push($select, "M.mid", "M.title", "M.year", "M.runningTime");
-		array_push($from, "movies M");
-
-		if ($_GET["search-title"]) {
-			array_push($where, "M.title like '%" . $_GET["search-title"] . "%'");
+		if ($_GET["search-name"]) {
+			array_push($where, "name like '" . $_GET["search-name"] . "'");
+			array_push($terms, "name '" . $_GET["search-name"] . "'");
 		}
-		
-		if ($_GET["search-year-start"]) {
-			array_push($where, "M.year>=" . $_GET["search-year-start"]);
+		if ($_GET["search-email"]) {
+			array_push($where, "email like '" . $_GET["search-email"] . "'");
+			array_push($terms, "email address '" . $_GET["search-email"] . "'");
 		}
-
-		if ($_GET["search-year-end"]) {
-			array_push($where, "M.year<=" . $_GET["search-year-end"]);
-		} 
-
-		if ($_GET["search-mpaarating"]) {
-			array_push($select, "MA.abbreviation");
-			array_push($from, "has_mpaa HA", "mpaaratings MA");
-			array_push($where, "M.mid=HA.mid", "HA.mpaaid=MA.mpaaid");
-			if ($_GET["search-mpaarating"] != "any") {
-				array_push($where, "MA.abbreviation like '" . $_GET["search-mpaarating"] . "'");
-			}
+		if ($_GET["search-age"]) {
+			array_push($where, "age=" . $_GET["search-age"]);
+			array_push($terms, "age " . $_GET["search-age"]);
+		}
+		if ($_GET["search-location"]) {
+			array_push($where, "location like '" . $_GET["search-location"] . "'");
+			array_push($terms, "located in '" . $_GET["search-location"] . "'");
+		}
+		if (isset($_GET["search-gender"]) && $_GET["search-gender"]) {
+			array_push($where, "gender like '" . $_GET["search-gender"] . "'");
+			array_push($terms, $_GET["search-gender"] . " gender");
 		}
 
-		if ($_GET["search-genre"]) {
-			array_push($select, "G.genre");
-			array_push($from, "has_genre HG", "genres G");
-			array_push($where, "M.mid=HG.mid", "HG.gid=G.gid", "G.genre like '" . $_GET["search-genre"] . "'");
+		$statement = "select * from users where " . join(" and ", $where) . ";";
+		$response = $db->query($statement);
+
+		echo "<h4>Search results for " . join(" and ", $terms) . "</h4> \n";
+
+		if ($response) {
+			?>
+			<table class='table table-striped table-hover'>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Age</th>
+						<th>Location</th>
+						<th>Gender</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						$response->data_seek(0);
+						while($row = $response->fetch_assoc()) {
+							echo "<tr> \n";
+							echo "<td><a href='user.php?u='" . $row["uid"] . "'>" . $row["name"] . "</a></td> \n";
+							echo "<td>" . $row["email"] . "</td> \n";
+							echo "<td>" . $row["age"] . "</td> \n";
+							echo "<td>" . $row["location"] . "</td> \n";
+							echo "<td>" . $row["gender"] . "</td> \n";
+							echo "</tr> \n";
+						}
+					?>
+				</tbody>
+			</table>
+			<?php
+		} else {
+			?>
+			<div class='text-center'>
+				<h2>No results found!</h2>
+				<p>Better luck next time</p>
+			</div>
+			<?php
 		}
-
-		if ($_GET["search-keyword"]) {
-			array_push($select, "K.keyword");
-			array_push($from, "has_key HK", "keywords K");
-			array_push($where, "M.mid=HK.mid", "HK.kid=K.kid", "K.keyword like '%" . $_GET["search-keyword"] . "%'");
-		}
-
-		if ($_GET["search-avgrating-low"] || $_GET["search-avgrating-high"]) {
-			array_push($select, "round(avg(R.rating), 2)");
-			array_push($from, "rated R");
-			array_push($where, "M.mid=R.mid");
-			array_push($groupby, "mid");
-		
-			if ($_GET["search-avgrating-low"]) {
-				array_push($having, "round(avg(R.rating), 2)>=" . $_GET["search-avgrating-low"]);
-			}
-			if ($_GET["search-avgrating-high"]) {
-				array_push($having, "round(avg(R.rating), 2)<=" . $_GET["search-avgrating-high"]);
-			}
-		}
-
-		if ($_GET["search-numrating-low"] || $_GET["search-numrating-high"]) {
-			array_push($select, "count(R.rating)");
-			
-			if (!in_array("rated R", $from)) {
-				array_push($from, "rated R");
-				array_push($where, "M.mid=R.mid");
-				array_push($groupby, "mid");
-			}
-
-			if ($_GET["search-numrating-low"]) {
-				array_push($having, "count(R.rating)>=" . $_GET["search-numrating-low"]);
-			}
-
-			if ($_GET["search-numrating-high"]) {
-				array_push($having, "count(R.rating)<=" . $_GET["search-numrating-high"]);
-			}
-		}
-
-		$query = "select " . join(", ", $select) . " from " . join(", ", $from) . " where " . join(" and ", $where);
-		if ($groupby) {
-			$query .= " group by " . join(", ", $groupby);
-		}
-		if ($having) {
-			$query .= " having " . join(" and ", $having);
-		}
-		if ($orderby) {
-			$query .= " order by " . join(", ", $orderby);
-		}
-		$query .= ";";
-		//echo $query;
-	
-		$response = $db->query($query);
-
-		$search = "";
-
-		if (!$response || $response->num_rows == 0) {
-                        $search .= "<div class='text-center'> \n";
-                        $search .= "<h2>No results found!</h2> \n";
-                        $search .= "<p>Try using less search terms</p> \n";
-                        $search .= "</div> \n";
-                } else {
-
-			$search .= "<div class='search-results'> \n";
-			$search .= "<h4>Search results for ";
-			
-			$terms = array();
-			$includes = array();
-			if ($_GET["search-title"]) {
-				array_push($terms, "title '" . $_GET["search-title"] . "'");
-			}
-			if ($_GET["search-mpaarating"]) {
-				array_push($terms, "MPAA rating '" . $_GET["search-mpaarating"] . "'");
-				array_push($includes, array("label" => "MPAA Rating", "field" => "abbreviation"));
-			}
-			if ($_GET["search-genre"]) {
-				array_push($terms, "genre '" . $_GET["search-genre"] . "'");
-				array_push($includes, array("label" => "Genre", "field" => "genre"));
-			}
-			if ($_GET["search-keyword"]) {
-				array_push($terms, "keyword '" . $_GET["search-keyword"] . "'");
-				array_push($includes, array("label" => "Keyword", "field" => "keyword"));
-			}
-			if ($_GET["search-year-start"] && $_GET["search-year-end"]) {
-				array_push($terms, "year between " . $_GET["search-year-start"] . " and " . $_GET["search-year-end"]);
-			} else if ($_GET["search-year-start"]) {
-				array_push($terms, "year after " . $_GET["search-year-start"]);
-			} else if ($_GET["search-year-end"]) {
-				array_push($terms, "year before " . $_GET["search-year-end"]);
-			}
-
-			if ($_GET["search-avgrating-low"] || $_GET["search-avgrating-high"]) {
-				array_push($includes, array("label" => "Average Rating", "field" => "round(avg(R.rating), 2)"));
-			}
-			if ($_GET["search-avgrating-low"] && $_GET["search-avgrating-high"]) {
-				array_push($terms, "average rating between " . $_GET["search-avgrating-low"] . " and " . $_GET["search-avgrating-high"]);
-			} else if ($_GET["search-avgrating-low"]) {
-				array_push($terms, "average rating higher than " . $_GET["search-avgrating-low"]);
-			} else if ($_GET["search-avgrating-high"]) {
-				array_push($terms, "average rating lower than " . $_GET["search-avgrating-high"]);
-			}
-
-			if ($_GET["search-numrating-low"] || $_GET["search-numrating-high"]) {
-				array_push($includes, array("label" => "Number of ratings", "field" => "count(R.rating)"));
-			}
-			if ($_GET["search-numrating-low"] && $_GET["search-numrating-high"]) {
-                                array_push($terms, "between " . $_GET["search-numrating-low"] . " and " . $_GET["search-numrating-high"] . " ratings");
-                        } else if ($_GET["search-numrating-low"]) {
-                                array_push($terms, "more than " . $_GET["search-numrating-low"] . " ratings");
-                        } else if ($_GET["search-numrating-high"]) {
-                                array_push($terms, "less than " . $_GET["search-numrating-high"] . " ratings");
-                        }
-
-
-			$search .= join(" and ", $terms);
-			$search .= "</h4> \n";
-
-			$search .= "<table class='table table-striped table-hover'> \n";
-			$search .= "<thead> \n";
-			$search .= "<tr> \n";
-			$search .= "<th>Title</th><th>Year</th>";
-			for ($i = 0; $i < count($includes); $i++) {
-				$search .= "<th>" . $includes[$i]["label"] . "</th>";
-			}
-			$search .= "</tr> \n";
-			$search .= "</thead> \n";
-
-			$search .= "<tbody> \n";
-
-			$response->data_seek(0);
-                        while ($row = $response->fetch_assoc()) {
-				$search .= "<tr> \n";
-				$search .= "<td><a href='movie.php?m=" . $row["mid"] . "'>" . $row["title"] . "</a></td><td>" . $row["year"] . "</td>";
-				foreach ($includes as $include) {
-					$search .= "<td>" . $row[$include["field"]] . "</td>";
-				}
-				$search .= "</tr> \n";
-                        }
-
-			$search .= "</tbody> \n";
-
-			$search .= "</table> \n";
-
-			$search .= "</div> \n";
-
-		}
-
-		echo $search;
 	}
 }
 
